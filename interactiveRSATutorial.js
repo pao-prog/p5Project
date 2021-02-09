031d6ffb2ba27573c63868c84516e380fe58ce2c
Paola Noun <paolanoun@berkeley.edu>
2021-02-08 21:02:25 -0800

    initialized p5Project

---
+++ interactiveRSATutorial.js
@@ -0,0 +1,21 @@
+
+function setup() {
+    createCanvas(800, 800);
+  }
+  
+  function draw() {
+    var toEncrypt = window.prompt("Alice enter your secret code: ");
+  }
+
+function keyGeneration() {
+
+}
+
+function encryption() {
+
+}
+
+function decryption() {
+    
+}
+  
